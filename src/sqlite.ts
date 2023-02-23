92432112245f37fd0dbc96eca132809f0bcd1cce
Leilani Battle <leibatt@cs.washington.edu>
2023-02-22 19:38:44 -0800

    testing sqlite. collecting initial test datasets

--- src/sqlite.ts
+++ src/sqlite.ts
@@ -1,9 +1,10 @@
 import { Database } from 'sqlite3';
 
+/*
 const db = new Database(':memory:');
 
 db.serialize(() => {
-    db.run("CREATE TABLE lorem (info TEXT)");
+    db.run("CREATE TABLE lorem (col TEXT)");
 
     const stmt = db.prepare("INSERT INTO lorem VALUES (?)");
     for (let i = 0; i < 10; i++) {
@@ -11,9 +12,23 @@ db.serialize(() => {
     }
     stmt.finalize();
 
-    db.each("SELECT rowid AS id, info FROM lorem", (err, row) => {
-        console.log(row.id + ": " + row.info);
+    executeQuery(db,"SELECT rowid AS id, col FROM lorem")
+    .then(rows => {
+        rows.forEach(row => { console.log(row.id + ": " + row.col); });
     });
 });
 
 db.close();
+*/
+
+export function executeQuery(db: Database, query: string): Promise<Record<string, any>[]> {
+  return new Promise((resolve, reject) => {
+    db.all(query,{}, (err, rows) => {
+      if(err) {
+        reject(err);
+      } else {
+        resolve(rows);
+      }
+    });
+  });
+}
